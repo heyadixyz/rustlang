44fed37566af6bdb188373ce4b3f874085013d49
Aditya <aditya@MacBookAir.lan>
2024-10-19 16:02:45 +0530

    1st module

---
+++ hello_world/hello_cargo/target/debug/deps/hello_cargo-440aadf8a8855801.d
@@ -0,0 +1,5 @@
+/Users/aditya/Desktop/rust-lang/hello_world/hello_cargo/target/debug/deps/hello_cargo-440aadf8a8855801: src/main.rs
+
+/Users/aditya/Desktop/rust-lang/hello_world/hello_cargo/target/debug/deps/hello_cargo-440aadf8a8855801.d: src/main.rs
+
+src/main.rs:
---
+++ hello_world/hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/aditya/Desktop/rust-lang/hello_world/hello_cargo/target/debug/hello_cargo: /Users/aditya/Desktop/rust-lang/hello_world/hello_cargo/src/main.rs
